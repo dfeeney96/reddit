3b152769c4257aff6f8e49fadcdce78d80a2daa9
dfeeney96 <daniellefeeney96@gmail.com>
2021-11-03 20:11:15 +0000

    add form

---
+++ src/components/searchTool.js
@@ -0,0 +1,16 @@
+function SearchTool () {
+
+    return(
+        <div>
+            <h1> Meme Central </h1>
+            <form>
+                <input type = "text"/>
+                <input type = "submit" />
+            </form>
+        </div>
+    )
+
+
+}
+
+export default SearchTool;
